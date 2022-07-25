70c93de5fc63fd98100fe78f78149726f6bb1b43
ideskangel <williamx@deskangel.com>
2022-07-25 20:33:04 +0800

    + git ignores and git-crypt rules

--- bin/dafvm.dart
+++ bin/dafvm.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:dafvm/dafvm.dart' as dafvm;
 import 'package:dafvm/dalaunch.dart' as dalaunch;
+import 'package:dafvm/dagit.dart' as dagit;
 
 void main(List<String> arguments) {
   if (arguments.isEmpty) {
@@ -25,6 +26,21 @@ path    Path to the project root
     exitCode = 2;
   } else {
     print('** Succeeded to create .vscode/launch.json');
+  }
+
+
+  if (!dagit.mergeGitIgnore(path)) {
+    print('- Failed to merge .gitignore');
+    exitCode = 2;
+  } else {
+    print('** Succeeded to merge .gitignore');
+  }
+
+  if (!dagit.createGitAttributes(path)) {
+    print('- Failed to create .gitattributes');
+    exitCode = 2;
+  } else {
+    print('** Succeeded to create .gitattributes.\\DO NOT forget to run `git-crypt init`.');
   }
 
   exitCode = 0;
---
+++ lib/dagit.dart
@@ -0,0 +1,58 @@
+import 'dart:developer' as dev;
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+
+const gitignore = '''
+!CHANGES.log
+
+pubspec.lock
+.fvm/flutter_sdk
+.build_number
+**/debug_info/
+macos/Flutter/GeneratedPluginRegistrant.swift
+''';
+
+const gitattr = '''
+# Specify filepatterns you want to assign special attributes.
+
+*.jks filter=git-crypt diff=git-crypt
+*.pem filter=git-crypt diff=git-crypt
+key.properties filter=git-crypt diff=git-crypt
+''';
+
+bool mergeGitIgnore(String path) {
+  var dir = Directory(path);
+  dev.log(dir.path);
+  if (!dir.existsSync()) {
+    print('path not exists');
+    return false;
+  }
+
+  var file = File(p.join(path, ".gitignore"));
+  if (file.existsSync()) {
+    file.writeAsStringSync(gitignore, mode: FileMode.append, flush: true);
+
+    return true;
+  }
+
+  return false;
+}
+
+bool createGitAttributes(String path) {
+  var dir = Directory(path);
+  dev.log(dir.path);
+  if (!dir.existsSync()) {
+    print('path not exists');
+    return false;
+  }
+
+  var file = File(p.join(path, ".gitattributes"));
+  if (!file.existsSync()) {
+    file.writeAsStringSync(gitattr, flush: true);
+    return true;
+  }
+
+  // do not modify the existed launch file
+  return false;
+}
