08fe9c26c56c0579b34eeba0974708417156f4de
ideskangel.m <dev@deskangel.com>
2025-04-05 16:31:05 +0800

    * fixed the project name issue in the build script

--- lib/dabuild.dart
+++ lib/dabuild.dart
@@ -1,12 +1,15 @@
 import 'dart:io';
 import 'dart:developer' as dev;
+import 'package:dafvm/global.dart';
 import 'package:path/path.dart' as p;
 
-const buildScript = '''
+final String buildScript = '''
 #!/bin/bash
 
 BUILD_NUMBER=\$(cat .build_number)
 BUILD_NUMBER=\$((BUILD_NUMBER + 1))
+
+APP_NAME=$projectName
 
 YEAR_NUMBER=\$(date +"%Y")
 
@@ -125,8 +128,8 @@ elif [[ "\$OPT_APP_FORMAT" == "linux" ]]; then
     # Update the build number
     if [[ \$BUILD_RESULT == 0 ]]; then
         VERSION=\$(grep -oP 'version:\\s*\\K\\d+(\\.\\d+){2}' pubspec.yaml)
-        sed_replace_regex "s/Version=.*\$/Version=\$VERSION/g" linux/assets/finutler.desktop
-        # update the version in finutler.desktop
+        sed_replace_regex "s/Version=.*\$/Version=\$VERSION/g" linux/assets/${projectName.toLowerCase()}.desktop
+        # update the version in ${projectName.toLowerCase()}.desktop
         echo \$BUILD_NUMBER >.build_number
     else
         clrecho "Failed to build the \$OPT_APP_FORMAT"
@@ -136,16 +139,16 @@ fi
 if [[ \$BUILD_RESULT == 0 && \$OPT_DEPLOYMENT == "-d" ]]; then
     if [[ "\$OPT_APP_FORMAT" == "macos" ]]; then
         echo "deploy to ~/Applications"
-        rm -rf ~/Applications/Finutler.app
-        cp -r build/macos/Build/Products/Release/Finutler.app ~/Applications/
+        rm -rf "~/Applications/\$APP_NAME.app"
+        cp -r "build/macos/Build/Products/Release/\$APP_NAME.app" ~/Applications/
     elif [[ "\$OPT_APP_FORMAT" == "linux" ]]; then
-        echo "deploy to /opt/Finutler"
-        sudo mv /opt/Finutler /opt/Finutler.bk
+        echo "deploy to /opt/\$APP_NAME"
+        sudo mv "/opt/\$APP_NAME" "/opt/\$APP_NAME.bk"
 
-        cp -f linux/assets/finutler.png ~/.local/share/icons/finutler.png
-        sudo cp -f linux/assets/finutler.desktop /usr/share/applications/finutler.desktop
-        sudo cp -r build/linux/x64/release/bundle /opt/Finutler
-        sudo rm -rf /opt/Finutler.bk
+        cp -f linux/assets/${projectName.toLowerCase()}.png ~/.local/share/icons/${projectName.toLowerCase()}.png
+        sudo cp -f linux/assets/${projectName.toLowerCase()}.desktop /usr/share/applications/${projectName.toLowerCase()}.desktop
+        sudo cp -r build/linux/x64/release/bundle "/opt/\$APP_NAME"
+        sudo rm -rf "/opt/\$APP_NAME.bk"
     else
         scp2p30p build/app/outputs/apk/release/app-arm64-v8a-release.apk /root/download/
     fi
@@ -165,7 +168,7 @@ bool createBuildScript(String path) {
 
   final file = File(p.join(path, 'release_build.sh'));
   if (file.existsSync()) {
-    print('\t- release_build.sh seems already exist.');
+    print('\t- ${file.path} seems already exist.');
     return false;
   }
 
@@ -185,7 +188,7 @@ bool _createBuildNumberFile(String path) {
 
   final file = File(p.join(path, '.build_number'));
   if (file.existsSync()) {
-    print('.build_number seems already exist.');
+    print('\t- ${file.path} seems already exist.');
     return false;
   }
 
